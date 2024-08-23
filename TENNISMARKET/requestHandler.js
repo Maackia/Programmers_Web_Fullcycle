cfe8f2e2e6bb09a9c7dd9873bc659564cb052857
Maackia <maackia.flos@gmail.com>
2024-08-23 16:13:30 +0900

    TennisMarket

--- TENNISMARKET/requestHandler.js
+++ TENNISMARKET/requestHandler.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const main_view = fs.readFileSync("./main.html", "utf8");
+const orderlist_view = fs.readFileSync("./orderlist.html", "utf8");
 
 const mariadb = require("./database/connect/mariadb");
 
@@ -13,6 +14,22 @@ function main(response) {
     response.write(main_view);
     response.end();
 }
+function orderlist(response) {
+    console.log("orderlist");
+
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(orderlist_view);
+
+    mariadb.query("SELECT * FROM orderlist", function (err, rows) {
+        rows.forEach((element) => {
+            response.write(
+                "<tr>" + "<td>" + element.product_id + "</td>" + "<td>" + element.order_date + "</td>" + "</tr>"
+            );
+        });
+        response.write("</table>");
+        response.end();
+    });
+}
 
 function maincss(response) {
     fs.readFile("./main.css", function (err, data) {
@@ -24,28 +41,45 @@ function maincss(response) {
 
 function redRacket(response) {
     fs.readFile("./img/redRacket.png", function (err, data) {
-        response.writeHead(200, { "Content-Type": "text/html" });
+        response.writeHead(200, { "Content-Type": "image/png" });
         response.write(data);
         response.end();
     });
 }
 function blueRacket(response) {
     fs.readFile("./img/blueRacket.png", function (err, data) {
-        response.writeHead(200, { "Content-Type": "text/html" });
+        response.writeHead(200, { "Content-Type": "image/png" });
         response.write(data);
         response.end();
     });
 }
 function blackRacket(response) {
     fs.readFile("./img/blackRacket.png", function (err, data) {
-        response.writeHead(200, { "Content-Type": "text/html" });
+        response.writeHead(200, { "Content-Type": "image/png" });
         response.write(data);
         response.end();
     });
 }
 
+function order(response, productId) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+
+    mariadb.query(
+        "INSERT INTO orderlist (product_id, order_date) VALUES (?, ?)",
+        [productId, new Date().toLocaleDateString()],
+        function (err, rows) {
+            console.log(rows);
+        }
+    );
+
+    response.write("order page");
+    response.end();
+}
+
 let handle = {}; // key: value
 handle["/"] = main;
+handle["/order"] = order;
+handle["/orderlist"] = orderlist;
 
 /* image directory */
 handle["/img/redRacket.png"] = redRacket;
--- TENNISMARKET/router.js
+++ TENNISMARKET/router.js
@@ -1,8 +1,8 @@
-function route(pathname, handle, response) {
+function route(pathname, handle, response, productId) {
     console.log("pathname: " + pathname);
 
     if ((typeof handle[pathname]).includes("function")) {
-        handle[pathname](response);
+        handle[pathname](response, productId);
     } else {
         response.writeHead(404, { "content-type": "text/html" });
         response.write("Not Found");
