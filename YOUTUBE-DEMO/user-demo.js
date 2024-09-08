af058815cea72623a7b59339c79ad6ffed0f9dad
Maackia <maackia.flos@gmail.com>
2024-09-08 23:31:50 +0900

    Add YOUTUBER-DEMO

---
+++ YOUTUBE-DEMO/user-demo.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.listen(8080);
+
+app.get("/", (req, res) => {
+    res.send("Hello, Express!");
+});
+
+// Login
+
+// Register
+
+// User Info
+
+// User Out
