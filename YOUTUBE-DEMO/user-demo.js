772d2f203ee8765ac6ba1d981fccacc7eec5f724
Maackia <maackia.flos@gmail.com>
2024-09-13 08:25:30 +0900

    Update user-demo.js

--- YOUTUBE-DEMO/user-demo.js
+++ YOUTUBE-DEMO/user-demo.js
@@ -7,12 +7,45 @@ app.use(express.json());
 let db = new Map();
 let id = 1;
 
+function isEmpty(obj) {
+    if (obj.constructor === Object) {
+        if (Object.keys(obj).length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
 app.get("/", (req, res) => {
     res.send("Main Page of Youtube-Demo Project.");
 });
 
 // Login
-app.post("/login", (req, res) => {});
+app.post("/login", (req, res) => {
+    const { userId, password } = req.body;
+    let loginUser = {};
+
+    db.forEach((user, id) => {
+        // a: value, b: key, c: Map
+        if (user.userId === userId) {
+            loginUser = user;
+        }
+    });
+
+    if (isEmpty(loginUser) != true) {
+        console.log("userId Found");
+
+        // Checking Password
+        if (loginUser.password === password) {
+            console.log("Pass Password");
+        } else {
+            console.log("Wrong Password");
+        }
+    } else {
+        console.log("입력하신 아이디를 찾을 수 없습니다.");
+    }
+});
 
 // Register
 app.post("/register", (req, res) => {
