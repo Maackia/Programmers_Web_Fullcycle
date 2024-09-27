a94f7f726caef04b14aeaaeb7d2bd43c33a808bc
Maackia <maackia.flos@gmail.com>
2024-09-27 19:45:44 +0900

    Add: db-demo.js

---
+++ NODE-BASE/db-demo.js
@@ -0,0 +1,20 @@
+// Get the client
+const mysql = require("mysql2");
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    database: "Youtube",
+    dateStrings: true,
+});
+
+// A simple SELECT query
+connection.query("SELECT * FROM `users`", function (err, results, fields) {
+    let { id, email, name, created_at } = results[0];
+    console.log(id);
+    console.log(email);
+    console.log(name);
+    console.log(created_at);
+});
