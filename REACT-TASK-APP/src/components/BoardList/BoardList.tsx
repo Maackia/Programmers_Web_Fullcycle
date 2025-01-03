f8b3172de32df7fa8266794a4a46cd75c0a6fa38
Maackia <maackia.flos@gmail.com>
2025-01-03 18:55:59 +0900

    Update: Week 12, Part 02 - 4

--- REACT-TASK-APP/src/components/BoardList/BoardList.tsx
+++ REACT-TASK-APP/src/components/BoardList/BoardList.tsx
@@ -1,7 +1,9 @@
-import React, { FC, useState } from "react";
+import clsx from "clsx";
+import React, { FC, useRef, useState } from "react";
 import { useTypedSelector } from "../../hooks/redux";
 import SideForm from "./SideForm/SideForm";
 import { FiPlusCircle } from "react-icons/fi";
+import { addButton, addSection, boardItem, boardItemActive, container, title } from "./BoardList.css";
 
 type TBoardListProps = {
     activeBoardId: string;
@@ -9,22 +11,41 @@ type TBoardListProps = {
 };
 
 const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId }) => {
-    const boardArray = useTypedSelector((state) => state.boards.boardArray);
+    const { boardArray } = useTypedSelector((state) => state.boards);
     const [isFormOpen, setIsFormOpen] = useState(false);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleClick = () => {
+        setIsFormOpen(!isFormOpen);
+        setTimeout(() => {
+            inputRef.current?.focus();
+        }, 0);
+    };
 
     return (
-        <div>
-            <div>게시판:</div>
+        <div className={container}>
+            <div className={title}>게시판:</div>
             {boardArray.map((board, index) => (
-                <div key={board.boardId}>
+                <div
+                    key={board.boardId}
+                    className={clsx(
+                        {
+                            [boardItemActive]: boardArray.findIndex((b) => b.boardId === activeBoardId) === index,
+                        },
+                        {
+                            [boardItem]: boardArray.findIndex((b) => b.boardId === activeBoardId) !== index,
+                        }
+                    )}
+                    onClick={() => setActiveBoardId(boardArray[index].boardId)}
+                >
                     <div>{board.boardName}</div>
                 </div>
             ))}
-            <div>
+            <div className={addSection}>
                 {isFormOpen ? (
-                    <SideForm setIsFormOpen={setIsFormOpen} />
+                    <SideForm inputRef={inputRef} setIsFormOpen={setIsFormOpen} />
                 ) : (
-                    <FiPlusCircle onClick={() => setIsFormOpen(!isFormOpen)} />
+                    <FiPlusCircle className={addButton} onClick={handleClick} />
                 )}
             </div>
         </div>
